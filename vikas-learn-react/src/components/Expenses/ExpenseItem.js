e4af3b15ddce7f6da364f178398960da136c6cb0
Vikas.Kumar <vikas.kumar@target.com>
2021-09-17 14:48:11 +0530

    Working with States

--- vikas-learn-react/src/components/Expenses/ExpenseItem.js
+++ vikas-learn-react/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,21 @@
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   function clickHandler() {
-    console.log("Clicked");
+    setTitle("Updated");
+    console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
