7b989c16f32ed2ce5c361eb016be99fb011cc191
Vikas.Kumar <vikas.kumar@target.com>
2021-12-02 16:40:08 +0530

    Add Base Data for students-marks schema

--- database/postgres/student.sql
+++ database/postgres/student.sql
@@ -40,3 +40,83 @@ SET city = 'Mumbai'
 where name = 'Rakesh';
 
 select * from students s;
+
+
+
+
+create TABLE IF NOT EXISTS Subjects(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO Subjects (name)
+VALUES
+    ('Maths'),
+    ('Science'),
+    ('English'),
+    ('Hindi'),
+    ('CS');
+   
+select * from subjects sbj;
+
+
+
+
+
+
+
+create TABLE IF NOT EXISTS Marks(
+    student_id SERIAL REFERENCES students(id) on delete cascade,
+    subject_id SERIAL REFERENCES subjects(id) on delete cascade,
+    marks INT,
+    primary key(student_id, subject_id)
+);
+
+drop table Marks;
+
+create TABLE IF NOT EXISTS Marks(
+    student_id INT REFERENCES students(id) on delete cascade,
+    subject_id INT REFERENCES subjects(id) on delete cascade,
+    marks INT,
+    primary key(student_id, subject_id)
+);
+
+
+INSERT INTO Marks (student_id, subject_id, marks)
+VALUES
+    (1, 1, 100),
+    (1, 2, 90),
+    (1, 3, 80),
+    (1, 4, 90),
+    (1, 5, 95),
+    (2, 1, 70),
+    (2, 2, 80),
+    (2, 3, 90),
+    (2, 4, 90),
+    (2, 5, 90),
+    (3, 1, 50),
+    (3, 2, 45),
+    (3, 3, 60),
+    (3, 4, 55),
+    (3, 5, 33),
+    (4, 1, 90),
+    (4, 2, 45),
+    (4, 3, 85),
+    (4, 4, 67),
+    (4, 5, 37),
+    (5, 1, 99),
+    (5, 2, 76),
+    (5, 3, 34),
+    (5, 4, 56),
+    (5, 5, 78),
+    (6, 1, 100),
+    (6, 2, 100),
+    (6, 3, 92),
+    (6, 4, 95),
+    (6, 5, 100);
+ 
+select * from marks m;
+
+
+
+
