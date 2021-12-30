c8071d85773462444bbe235d1673fdc62c3d376e
Vikas.Kumar <vikas.kumar@target.com>
2021-12-30 12:24:44 +0530

    Completed Joins SQL

--- database/postgres/tutorial/3.Joins.sql
+++ database/postgres/tutorial/3.Joins.sql
@@ -25,6 +25,7 @@ VALUES
     (4, 'Pear');
     
 
+   
 -- Inner Join
 
 select *
@@ -33,6 +34,36 @@ from
 inner join basket_b
     on fruit_a = fruit_b;
    
+
+SELECT
+	customer_id,
+	first_name,
+	last_name,
+	amount,
+	payment_date
+FROM
+	customer
+INNER JOIN payment USING(customer_id)
+ORDER BY payment_date;
+
+
+SELECT
+	c.customer_id,
+	c.first_name customer_first_name,
+	c.last_name customer_last_name,
+	s.first_name staff_first_name,
+	s.last_name staff_last_name,
+	amount,
+	payment_date
+FROM
+	customer c
+INNER JOIN payment p 
+    ON p.customer_id = c.customer_id
+INNER JOIN staff s 
+    ON p.staff_id = s.staff_id
+ORDER BY payment_date;
+   
+
 
 -- Left / Left Outer Join
    
@@ -54,6 +85,18 @@ from
 left outer join basket_b
     on fruit_a = fruit_b
 where b is null;
+
+
+SELECT
+	f.film_id,
+	title,
+	inventory_id
+FROM
+	film f
+LEFT JOIN inventory i USING (film_id)
+WHERE i.film_id IS NULL
+ORDER BY title;
+
 
 
 -- Right / Right Outer Join
@@ -78,6 +121,7 @@ right outer join basket_b
 where a is null;
 
 
+
 -- Full / Full Outer Join
 
 select *
@@ -100,6 +144,7 @@ full outer join basket_b
 where a is null or b is null;
 
 
+
 -- Table Aliases
 select ba.a, ba.fruit_a, bb.b, bb.fruit_b 
 from 
@@ -107,6 +152,7 @@ from
 full outer join basket_b bb 
     on ba.fruit_a = bb.fruit_b
 where ba.a is null or bb.b is null;
+
 
 -- Table Aliases in Self Join
 SELECT
@@ -119,4 +165,107 @@ INNER JOIN employee m
 ORDER BY manager;
 
 
--- 
+-- Self Join
+
+SELECT select_list
+FROM table_name t1
+INNER JOIN table_name t2 ON join_predicate;
+
+SELECT select_list
+FROM table_name t1
+LEFT JOIN table_name t2 ON join_predicate;
+
+
+CREATE TABLE employee (
+	employee_id INT PRIMARY KEY,
+	first_name VARCHAR (255) NOT NULL,
+	last_name VARCHAR (255) NOT NULL,
+	manager_id INT,
+	FOREIGN KEY (manager_id) 
+	REFERENCES employee (employee_id) 
+	ON DELETE CASCADE
+);
+INSERT INTO employee (
+	employee_id,
+	first_name,
+	last_name,
+	manager_id
+)
+VALUES
+	(1, 'Windy', 'Hays', NULL),
+	(2, 'Ava', 'Christensen', 1),
+	(3, 'Hassan', 'Conner', 1),
+	(4, 'Anna', 'Reeves', 2),
+	(5, 'Sau', 'Norman', 2),
+	(6, 'Kelsie', 'Hays', 3),
+	(7, 'Tory', 'Goff', 3),
+	(8, 'Salley', 'Lester', 3);
+
+
+SELECT
+    e.first_name || ' ' || e.last_name employee,
+    m .first_name || ' ' || m .last_name manager
+FROM
+    employee e
+INNER JOIN employee m ON m .employee_id = e.manager_id
+ORDER BY manager;
+
+SELECT
+    e.first_name || ' ' || e.last_name employee,
+    m .first_name || ' ' || m .last_name manager
+FROM
+    employee e
+LEFT JOIN employee m ON m .employee_id = e.manager_id
+ORDER BY manager;
+
+
+SELECT
+    f1.title,
+    f2.title,
+    f1.length
+FROM
+    film f1
+INNER JOIN film f2 
+    ON f1.film_id <> f2.film_id AND 
+       f1.length = f2.length;
+      
+      
+      
+-- Cross Join
+
+SELECT select_list
+FROM T1
+CROSS JOIN T2;
+
+SELECT select_list
+FROM T1, T2;
+
+SELECT *
+FROM T1
+INNER JOIN T2 ON true;
+
+
+DROP TABLE IF EXISTS T1;
+CREATE TABLE T1 (label CHAR(1) PRIMARY KEY);
+
+DROP TABLE IF EXISTS T2;
+CREATE TABLE T2 (score INT PRIMARY KEY);
+
+INSERT INTO T1 (label)
+VALUES
+	('A'),
+	('B');
+
+INSERT INTO T2 (score)
+VALUES
+	(1),
+	(2),
+	(3);
+
+SELECT *
+FROM T1
+CROSS JOIN T2;
+
+
+
+-- Natural Join
